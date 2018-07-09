707b801b42b5080f57f40f486d1c30b06585d0bc
Kannan Paramasivam <kannan.paramasivam@live.com>
2018-07-09 08:00:09 -0500

    added card for recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "Test recipe desc",
+      "https://hips.hearstapps.com/ghk.h-cdn.co/assets/18/10/1280x1280/square-1520278006-grilled-chicken-breast-vegetables.jpg?resize=640:*"),
+      new Recipe("Test new Recipe", "Test nre recipe desc",
+      "https://hips.hearstapps.com/ghk.h-cdn.co/assets/18/10/1280x1280/square-1520278006-grilled-chicken-breast-vegetables.jpg?resize=640:*")
+  ];
 
   constructor() { }
 
